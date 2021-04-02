110aee5d29dd54edee978ff61652c09ff2464f14
Patricio López Juri <lopezjuripatricio@gmail.com>
2021-04-02 16:45:58 -0300

    feat: Improve <head> of processed files

--- src/prepare.ts
+++ src/prepare.ts
@@ -107,7 +107,9 @@ export async function prepareProject({
 
             <html>
               <head>
-                <meta charset="utf-8" />
+                <meta charset="utf-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <link rel="preconnect" href="https://fonts.gstatic.com">
                 <title>${name}</title>
                 <style>
                   ${GLOBAL_STYLE}
@@ -170,7 +172,9 @@ export async function prepareProject({
 
             <html>
               <head>
-                <meta charset="utf-8" />
+                <meta charset="utf-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <link rel="preconnect" href="https://fonts.gstatic.com">
                 <title>${name}</title>
                 <style>
                   ${GLOBAL_STYLE}
