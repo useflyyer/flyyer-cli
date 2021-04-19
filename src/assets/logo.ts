48dc4f6c2097913004fec92371e9c68d2cdb6ff2
Patricio López Juri <lopezjuripatricio@gmail.com>
2021-04-19 09:02:50 -0400

    feat: Convert SVG logo to dataURI

--- src/assets/logo.ts
+++ src/assets/logo.ts
@@ -1,6 +1,5 @@
-// export const ORIGINAL = `<svg width="100" height="100" fill="none" xmlns="http://www.w3.org/2000/svg"><rect x="17" y="29" width="81" height="31" rx="5" fill="#5133FF"/><path d="M7 40.5h71c2 0 3.5 1.6 3.5 3.5v21c0 2-1.5 3.5-3.5 3.5H7c-2 0-3.5-1.6-3.5-3.5V44c0-2 1.6-3.5 3.5-3.5z" stroke="#fff" stroke-width="3"/><path d="M2 44a5 5 0 015-5h46v31H7a5 5 0 01-5-5V44z" fill="url(#paint0_linear)"/><defs><linearGradient id="paint0_linear" x1="27.5" y1="39" x2="27.5" y2="63.3" gradientUnits="userSpaceOnUse"><stop stop-color="#fff" stop-opacity="0"/><stop offset="1" stop-color="#fff" stop-opacity=".4"/></linearGradient></defs></svg>`;
+import svgToMiniDataURI from "mini-svg-data-uri";
 
-/**
- * https://npm.runkit.com/mini-svg-data-uri
- */
-export const ENCODED = `data:image/svg+xml,%3csvg width='100' height='100' fill='none' xmlns='http://www.w3.org/2000/svg'%3e%3crect x='17' y='29' width='81' height='31' rx='5' fill='%235133FF'/%3e%3cpath d='M7 40.5h71c2 0 3.5 1.6 3.5 3.5v21c0 2-1.5 3.5-3.5 3.5H7c-2 0-3.5-1.6-3.5-3.5V44c0-2 1.6-3.5 3.5-3.5z' stroke='white' stroke-width='3'/%3e%3cpath d='M2 44a5 5 0 015-5h46v31H7a5 5 0 01-5-5V44z' fill='url(%23paint0_linear)'/%3e%3cdefs%3e%3clinearGradient id='paint0_linear' x1='27.5' y1='39' x2='27.5' y2='63.3' gradientUnits='userSpaceOnUse'%3e%3cstop stop-color='white' stop-opacity='0'/%3e%3cstop offset='1' stop-color='white' stop-opacity='.4'/%3e%3c/linearGradient%3e%3c/defs%3e%3c/svg%3e`;
+export const ORIGINAL = `<svg width="100" height="100" fill="none" xmlns="http://www.w3.org/2000/svg"><rect x="17" y="29" width="81" height="31" rx="5" fill="#5133FF"/><path d="M7 40.5h71c2 0 3.5 1.6 3.5 3.5v21c0 2-1.5 3.5-3.5 3.5H7c-2 0-3.5-1.6-3.5-3.5V44c0-2 1.6-3.5 3.5-3.5z" stroke="#fff" stroke-width="3"/><path d="M2 44a5 5 0 015-5h46v31H7a5 5 0 01-5-5V44z" fill="url(#paint0_linear)"/><defs><linearGradient id="paint0_linear" x1="27.5" y1="39" x2="27.5" y2="63.3" gradientUnits="userSpaceOnUse"><stop stop-color="#fff" stop-opacity="0"/><stop offset="1" stop-color="#fff" stop-opacity=".4"/></linearGradient></defs></svg>`;
+
+export const ENCODED = svgToMiniDataURI(ORIGINAL);
--- src/prepare.ts
+++ src/prepare.ts
@@ -38,7 +38,7 @@ const FAVICON = ENCODED;
 const DEFAULT_TAGS = dedent`
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="icon" href="${FAVICON}" type="image/svg+xml">
+  <link rel="icon" href="${FAVICON}" sizes="any" type="image/svg+xml">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link rel="preconnect" href="https://fonts.googleapis.com" crossorigin>
   <link rel="preconnect" href="https://twemoji.maxcdn.com" crossorigin>
